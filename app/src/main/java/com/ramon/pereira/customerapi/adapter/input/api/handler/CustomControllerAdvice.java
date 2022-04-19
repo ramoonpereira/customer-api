d831943e8cb87c9403fe775baf66afa2bac3abb8
Ramon Pereira <ramon.pereira@luizalabs.com>
2022-04-19 15:34:49 -0300

    feat: create controller advice

---
+++ app/src/main/java/com/ramon/pereira/customerapi/adapter/input/api/handler/CustomControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.ramon.pereira.customerapi.adapter.input.api.handler;
+
+import com.ramon.pereira.customerapi.core.exception.ConflictCustomerException;
+import javax.persistence.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+class CustomControllerAdvice extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(value = {ConflictCustomerException.class,})
+  protected ResponseEntity<Object> handleConflict(
+      ConflictCustomerException ex, WebRequest request) {
+    return handleExceptionInternal(ex, ex.getMessage(),
+        new HttpHeaders(), HttpStatus.CONFLICT, request);
+  }
+
+  @ExceptionHandler(value = {EntityNotFoundException.class,})
+  protected ResponseEntity<Object> handleNotFound(
+      EntityNotFoundException ex, WebRequest request) {
+    return handleExceptionInternal(ex, ex.getMessage(),
+        new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  }
+
+}
