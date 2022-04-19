b4c45d1a2860b99a0f8a5333ff899f14c5d03612
Ramon Pereira <ramon.pereira@luizalabs.com>
2022-04-19 08:11:41 -0300

    feat: create repository

--- app/src/main/java/com/ramon/pereira/customerapi/CustomerApiApplication.java
+++ app/src/main/java/com/ramon/pereira/customerapi/CustomerApiApplication.java
@@ -2,12 +2,8 @@ package com.ramon.pereira.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EntityScan
 public class CustomerApiApplication {
 
 	public static void main(String[] args) {
--- app/src/main/java/com/ramon/pereira/customerapi/core/usecase/port/CustomerRepository.java
+++ app/src/main/java/com/ramon/pereira/customerapi/core/usecase/port/CustomerRepository.java
@@ -1,4 +1,16 @@
 package com.ramon.pereira.customerapi.core.usecase.port;
 
-public interface CustomerRepository {
+import com.ramon.pereira.customerapi.core.domain.Customer;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+
+  Optional<Customer> findByCpf(final Long cpf);
+
 }
